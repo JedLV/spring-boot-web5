67c978e358eee587225c927cb23776772c588c60
编程 <1660873558@qq.com>
2021-08-25 21:52:12 +0800

    Initial commit

---
+++ src/test/java/com/example/springbootweb2/SpringBootWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootweb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWeb2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
